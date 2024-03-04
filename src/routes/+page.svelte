d31fc2ed19b3bff77246451fd47d3850b598a7e5
aliasboink <adrianlaurb@gmail.com>
2024-03-04 14:04:43 +0200

    Add Svelte setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let data;
+	const posts = data.posts;
+</script>
+
+<div class="main-wrapper row top-xs red">
+	<div class="header-wrapper center-xs col-xs-12 blue">
+		<div class="header-content row middle-xs cyan">
+			<div class="col-xs-2 title-wrapper">
+				<div class="box">
+					<a href="https://www.adrian-docs.com/" class="title" title="Adrian Docs">Adrian Docs</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="posts-wrapper row center-xs top-xs col-xs-12 black">
+		<div class="post col-xs-8 red">
+			<div class="box"></div>
+		</div>
+	</div>
+</div>

c77503b21b303c098b35c70841be97781748ad03
aliasboink <adrianlaurb@gmail.com>
2024-03-17 16:39:38 +0200

    Add offset

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,8 @@
+let offset = 0
 export const load = async () => {
-  const response = await fetch('http://localhost:8080/v1/posts');
+  let limit = 15
+  const response = await fetch(`http://localhost:8080/v1/posts/${offset}`);
   const posts = await response.json();
+  offset = limit + 15
   return { posts };
 }
